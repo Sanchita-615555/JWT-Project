5fede6f77e9f1c4ca3a5f628ef71b0f0cbcf8c5d
Sanchita Sinha <sanchitasinha628@gmail.com>
2025-12-24 12:35:50 +0530

    Update server.js: use SECRET_KEY from env

--- back/server.js
+++ back/server.js
@@ -86,7 +86,7 @@ function auth(req, res, next) {
 
   const token = header.split(" ")[1];
   try {
-    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = jwt.verify(token, process.env.SECRET_KEY);
     next();
   } catch {
     res.status(401).json({ message: "Invalid token" });
